eb79cf962b55f469547d9333e421669308635adf
Cotto213 <alfonsocotto5b12@gmail.com>
2023-11-22 00:38:46 -0600

    Update CreateEditHealth.js
    Se agregó una función para calcular automaticamente el BMI

--- src/screens/BodyInfo/CreateEditHealth.js
+++ src/screens/BodyInfo/CreateEditHealth.js
@@ -1,5 +1,5 @@
 import { View, Text, ActivityIndicator } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Header } from '../../components/Header';
 import { AuthenticateLayout } from '../../layouts/AuthenticateLayout';
@@ -7,12 +7,10 @@ import { PrimaryButton } from '../../components/PrimaryButton';
 
 import { useFormik } from "formik";
 import * as Yup from 'yup';
-import { SelectInput } from '../../components/SelectInput';
 import { createEditHealth } from '../../hooks/HealthHook';
-import { TxtInput } from '../../components/TxtInput';
 import { FormikInput } from '../../components/FormikInput';
 
-export const CreateEditHealth = ({navigation,route}) => {
+export const CreateEditHealth = ({navigation, route}) => {
     const formik = useFormik({
         initialValues: {
             id: route.params.id ?? "",
@@ -34,10 +32,26 @@ export const CreateEditHealth = ({navigation,route}) => {
         
         onSubmit: async (health) => await createEditAttempt.mutateAsync(health),
     });
+
     const createEditAttempt = createEditHealth(formik.setErrors, formik.values);
 
-   
-    return(
+    useEffect(() => {
+        const weightInLB = parseFloat(formik.values.weight);
+        const heightInCM = parseFloat(formik.values.height);
+
+        if (!isNaN(weightInLB) && !isNaN(heightInCM) && heightInCM > 0) {
+            // Convertir peso de libras a kilogramos
+            const weightInKG = weightInLB * 0.453592;
+            // Convertir altura de centímetros a metros
+            const heightInM = heightInCM / 100;
+
+            // Calcular BMI
+            const bmi = (weightInKG / (heightInM * heightInM)).toFixed(2);
+            formik.setFieldValue('bmi', bmi);
+        }
+    }, [formik.values.weight, formik.values.height]);
+
+    return (
         <AuthenticateLayout>
             <Header navigation={navigation} />
 
@@ -47,9 +61,9 @@ export const CreateEditHealth = ({navigation,route}) => {
                         {formik.values.id == "" ? "Add new Body Health Information" : "Update Information"}
                     </Text>
                     
-                    <FormikInput placeholder={'Weight'} formik={formik} valueName={'weight'} label={formik.values.id == '' ? '':'Weight'}/>
-                    <FormikInput placeholder={'Height'} formik={formik} valueName={'height'} label={formik.values.id == '' ? '':'Height'}/>
-                    <FormikInput placeholder={'BMI'} formik={formik} valueName={'bmi'} label={formik.values.id == '' ? '':'BMI'}/>
+                    <FormikInput placeholder={'Weight in LB'} formik={formik} valueName={'weight'} label={formik.values.id == '' ? '':'Weight'}/>
+                    <FormikInput placeholder={'Height in CM'} formik={formik} valueName={'height'} label={formik.values.id == '' ? '':'Height'}/>
+                    <FormikInput placeholder={'BMI'} formik={formik} valueName={'bmi'} label={formik.values.id == '' ? '':'BMI'} editable={false} />
                     <FormikInput placeholder={'Blood Pressure'} formik={formik} valueName={'blood_pressure'} label={formik.values.id == '' ? '':'Blood Pressure'}/>
                     <FormikInput placeholder={'Blood Sugar'} formik={formik} valueName={'blood_sugar'} label={formik.values.id == '' ? '':'Blood Sugar'}/>
                     <FormikInput placeholder={'Date: fmt(YYYY-MM-DD HH:MM:SS)'} formik={formik} valueName={'date'} label={formik.values.id == '' ? '':'Date: fmt(YYYY-MM-DD HH:MM:SS)'}/>
@@ -62,5 +76,5 @@ export const CreateEditHealth = ({navigation,route}) => {
                 </View>
             </View>
         </AuthenticateLayout>
-    )
+    );
 }
